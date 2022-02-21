035d2a4312b1200be6307167c7d124b45ea45c94
blackstar-baba <535650957@qq.com>
2022-02-21 16:41:49 +0800

    fix subquery order not work

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/SubqueryRelation.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/SubqueryRelation.java
@@ -8,11 +8,6 @@ public class SubqueryRelation extends Relation {
     public SubqueryRelation(String name, QueryRelation query) {
         this.name = name;
         this.query = query;
-        // The order by is meaningless in subquery
-        if (this.query instanceof SelectRelation && !((SelectRelation) this.query).hasLimit()) {
-            SelectRelation qs = (SelectRelation) this.query;
-            qs.clearOrder();
-        }
     }
 
     public String getName() {
